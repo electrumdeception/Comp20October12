92ca2f51cb7ec55dfcb5e5eb75f501b0120b5b15
electrumdeception <59322785+electrumdeception@users.noreply.github.com>
2020-10-12 21:20:09 -0400

    Add files via upload

--- Hotdogorder.js
+++ Hotdogorder.js
@@ -6,7 +6,7 @@ function takeOrder(){
 	var soda = sodaCost();
 	var fries = fryCost();
 	var totalPrice = subtotal(soda, hotdog, fries);
-	addendum("AmounttoCharge", totalPrice);
+	addendum("finalTotal", "Total $".concat(totalPrice));
 }
 
 //Ask the user for the quantity of hotdogs, fries and sodas they want.
@@ -14,24 +14,29 @@ function fryCost()
 {
 	var frieCount;
 	frieCount = prompt("how many fries", 2);
-	addendum("fryCount", frieCount)
 	var fryCost = 2 * frieCount;
+	var stringToAdd = frieCount.concat(". Total is $",fryCost);
+	addendum("fryCount", stringToAdd);
 	return fryCost;
 }
 function hotDogCost()
 {
 	var dogCount;
-	dogCount = prompt("how many fries", 2);
-	addendum("dogCount", dogCount)
+	dogCount = prompt("how many hot dogs", 2);
+	//addendum("dogCount", dogCount)
 	var dogCost = 3.25 * dogCount;
+	var stringToAdd = dogCount.concat(". Total is $",dogCost);
+	addendum("dogCount", stringToAdd);
 	return dogCost;
 }
 function sodaCost()
 {
 	var sodaCount;
-	sodaCount = prompt("how many fries", 2);
-	addendum("sodaCount", sodaCount)
+	sodaCount = prompt("how many sodas", 2);
+	//addendum("sodaCount", sodaCount)
 	var sodaCost = 1.5 * sodaCount;
+	var stringToAdd = sodaCount.concat(". Total is $",sodaCost);
+	addendum("sodaCount", stringToAdd);
 	return sodaCost;
 }
 
@@ -42,11 +47,15 @@ function subtotal(soda,dog,fry)
 {
 	var cost = soda+dog+fry;
 	if (cost >20){
+		addendum("sale", "You get a 10% discount");
 		cost = cost*.9;
 	}
+	addendum("subtotal", "Subtotal is $".concat(cost.toFixed(2)));
+	var tax = cost * 0.0625;
+	addendum("tax","MA tax: $".concat(tax.toFixed(2)));
 	cost=cost*1.0625;
 	var roundedCost = cost.toFixed(2);
-return roundedCost;
+	return roundedCost;
 	
 }
 
